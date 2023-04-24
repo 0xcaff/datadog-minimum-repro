7d1ded4ae810ca16b48df4be1dbf10701e3dc88d
martin <martincharles07@gmail.com>
2023-04-23 23:39:38 -0500

    add message pack

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,7 @@
 import "isomorphic-fetch";
 import { AgentPayload } from "./gen/agent_payload_pb.js";
 import { gzip } from "node-gzip";
+import { Packr } from "msgpackr";
 import { webcrypto as crypto } from "node:crypto";
 
 async function main() {
@@ -17,39 +18,118 @@ const agentHostname = "test-host-name";
 const env = "prod";
 
 async function sendStats() {
+  const packr = new Packr();
   const response = await fetch(
     "https://trace.agent.datadoghq.com/api/v0.2/stats",
-    {
-      method: "POST",
-      headers: {
-        "X-Datadog-Reported-Languages": "javascript",
-        "content-type": "application/msgpack",
-        "content-encoding": "gzip",
-        ...sharedHeaders,
-      },
-      body: await gzip(
-        JSON.stringify({
-          agentHostname,
-          agentEnv: env,
-          agentVersion,
-          stats: [
-            {
-              stats: [
+      {
+        method: "POST",
+        headers: {
+          "X-Datadog-Reported-Languages": "javascript",
+          "content-type": "application/msgpack",
+          "content-encoding": "gzip",
+          ...sharedHeaders,
+        },
+        body: await gzip(
+            packr.pack(
                 {
-                  start: Number(BigInt(Date.now()) * 1000n),
-                  duration: Number(10n * 1000n * 1000n),
-                  stats: [
+                  "AgentHostname": "laptop",
+                  "AgentEnv": "none",
+                  "Stats": [
                     {
-                      hits: 3,
-                    },
+                      "Hostname": "laptop",
+                      "Env": "none",
+                      "Version": "1.0.0",
+                      "Stats": [
+                        {
+                          "Start": 1682309420000000000,
+                          "Duration": 10000000000,
+                          "Stats": [
+                            {
+                              "Service": "test",
+                              "Name": "test.test",
+                              "Resource": "test.test",
+                              "HTTPStatusCode": 0,
+                              "Type": "",
+                              "DBType": "",
+                              "Hits": 1,
+                              "Errors": 0,
+                              "Duration": 1002406250,
+                              "OkSummary": {
+                                "type": "Buffer",
+                                "data": [
+                                  10,
+                                  9,
+                                  9,
+                                  253,
+                                  74,
+                                  129,
+                                  90,
+                                  191,
+                                  82,
+                                  240,
+                                  63,
+                                  18,
+                                  13,
+                                  18,
+                                  8,
+                                  0,
+                                  0,
+                                  0,
+                                  0,
+                                  0,
+                                  0,
+                                  240,
+                                  63,
+                                  24,
+                                  152,
+                                  16,
+                                  26,
+                                  0
+                                ]
+                              },
+                              "ErrorSummary": {
+                                "type": "Buffer",
+                                "data": [
+                                  10,
+                                  9,
+                                  9,
+                                  253,
+                                  74,
+                                  129,
+                                  90,
+                                  191,
+                                  82,
+                                  240,
+                                  63,
+                                  18,
+                                  0,
+                                  26,
+                                  0
+                                ]
+                              },
+                              "Synthetics": false,
+                              "TopLevelHits": 1
+                            }
+                          ],
+                          "AgentTimeShift": 0
+                        }
+                      ],
+                      "Lang": "",
+                      "TracerVersion": "",
+                      "RuntimeID": "",
+                      "Sequence": 0,
+                      "AgentAggregation": "",
+                      "Service": "",
+                      "ContainerID": "",
+                      "Tags": []
+                    }
                   ],
-                },
-              ],
-            },
-          ],
-        })
-      ),
-    }
+                  "AgentVersion": "7.43.1",
+                  "ClientComputed": false
+                }
+            )
+        )
+      }
   );
 
   const body = await response.text();
@@ -118,13 +198,16 @@ function toNanos(date: Date) {
 }
 
 function millisToNanos(value: number) {
-  return BigInt(value) * 1000000n
+  return BigInt(value) * 1000000n;
 }
 
 function generateId() {
   const bytes = new Uint8Array(8);
   crypto.getRandomValues(bytes);
 
-  const bytesHex = bytes.reduce((acc, byte) => acc + byte.toString(16).padStart(2, '0'), '')
+  const bytesHex = bytes.reduce(
+    (acc, byte) => acc + byte.toString(16).padStart(2, "0"),
+    ""
+  );
   return BigInt(`0x${bytesHex}`);
 }
